c4c5840495c8854a27ad0ea6beebb52ec3831976
Deven Shah <devenshah1974@gmail.com>
2021-04-02 02:02:18 +0100

    :sparkles: Added logger

--- src/XTrimCalculator.ConsoleApp/Program.cs
+++ src/XTrimCalculator.ConsoleApp/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace XTrimCalculator.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using IHost host = CreateHostBuilder(args).Build();
+
+            var logger = host.Services.GetService<ILogger<Program>>();
+            logger.LogInformation("Hello World!");
+
+            await host.RunAsync();
         }
+
+        static IHostBuilder CreateHostBuilder(string[] args) => 
+            Host
+                .CreateDefaultBuilder(args)
+                .ConfigureLogging(l => 
+                {
+                    l.ClearProviders();
+                    l.AddConsole();
+                });
     }
 }
