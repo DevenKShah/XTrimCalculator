b8f6a0c8bb6987e4a3f7159bce7156f02e523852
Deven Shah <devenshah1974@gmail.com>
2021-04-05 21:43:49 +0100

    :ok_hand: Implement AutoMoq

--- src/XTrimCalculator.UnitTests/Application/CalculatorOrchestratorTests/GivenInvalidInstructions.cs
+++ src/XTrimCalculator.UnitTests/Application/CalculatorOrchestratorTests/GivenInvalidInstructions.cs
@@ -1,29 +1,22 @@
 ﻿using FluentAssertions;
 using FluentValidation;
 using FluentValidation.Results;
-using Microsoft.Extensions.Logging;
 using Moq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using XTrimCalculator.Application;
 using XTrimCalculator.Domain.Entities;
-using XTrimCalculator.Domain.Interfaces;
 using Xunit;
+using AutoFixture.Xunit2;
 
 namespace XTrimCalculator.UnitTests.Application.CalculatorOrchestratorTests
 {
     public class GivenInvalidInstructions
     {
-        [Fact]
-        public void When_Empty_Lines_Then_Return_Zero()
+        [Theory, AutoMoqData]
+        public void When_Empty_Lines_Then_Return_Zero(CalculatorOrchestrator sut)
         {
             //Arrange
-            var validatorMock = new Mock<IValidator<IEnumerable<Instruction>>>();
-            var calulatorServiceMock = new Mock<ICalculatorService>();
-            var loggerMock = new Mock<ILogger<CalculatorOrchestrator>>();
-            var sut = new CalculatorOrchestrator(validatorMock.Object, calulatorServiceMock.Object, loggerMock.Object);
 
             //Act
             var result = sut.Execute(new[] { " ", " " });
@@ -32,14 +25,10 @@ namespace XTrimCalculator.UnitTests.Application.CalculatorOrchestratorTests
             result.Result.Should().Be(0);
         }
 
-        [Fact]
-        public void When_Invalid_Lines_Then_Return_Error()
+        [Theory, AutoMoqData]
+        public void When_Invalid_Lines_Then_Return_Error([Frozen] Mock<IValidator<IEnumerable<Instruction>>> validatorMock, CalculatorOrchestrator sut)
         {
             //Arrange
-            var validatorMock = new Mock<IValidator<IEnumerable<Instruction>>>();
-            var calulatorServiceMock = new Mock<ICalculatorService>();
-            var loggerMock = new Mock<ILogger<CalculatorOrchestrator>>();
-            var sut = new CalculatorOrchestrator(validatorMock.Object, calulatorServiceMock.Object, loggerMock.Object);
             validatorMock
                 .Setup(v => v.Validate(It.IsAny<IEnumerable<Instruction>>()))
                 .Returns(new ValidationResult(new[] { new ValidationFailure("field","error message") } ));
@@ -53,14 +42,10 @@ namespace XTrimCalculator.UnitTests.Application.CalculatorOrchestratorTests
             result.Errors.Should().OnlyContain(c => c == "error message");
         }
 
-        [Fact]
-        public void When_Bad_Data_Then_Return_Error()
+        [Theory, AutoMoqData]
+        public void When_Bad_Data_Then_Return_Error(CalculatorOrchestrator sut)
         {
             //Arrange
-            var validatorMock = new Mock<IValidator<IEnumerable<Instruction>>>();
-            var calulatorServiceMock = new Mock<ICalculatorService>();
-            var loggerMock = new Mock<ILogger<CalculatorOrchestrator>>();
-            var sut = new CalculatorOrchestrator(validatorMock.Object, calulatorServiceMock.Object, loggerMock.Object);
 
             //Act
             var result = sut.Execute(new[] { "ad 1", "subtract 2" });
